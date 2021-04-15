c770aa0b8a5bf91c6c6c27444bfce370b270fc3a
grimmalirs <ksenof.serrr@mail.ru>
2021-04-15 14:42:41 +0300

    Json support added

--- lespace_Kirov_Mikhaylov_Ksenofontov/myform.py
+++ lespace_Kirov_Mikhaylov_Ksenofontov/myform.py
@@ -2,6 +2,7 @@ from bottle import post, request
 
 import json
 import re
+import os
 #import pdb
 @post('/home', method='post')
 
@@ -9,17 +10,16 @@ def my_form():
     mail = request.forms.get('ADRESS')
     Question = request.forms.get('QUEST')
     Hystory = {}
-    data = {}
-    Hystory[mail] = Question
-    with open('data.txt') as f:
-        data = json.load(f)
-    newData = data.update(Hystory)
     pattern=r"^\w+[@][a-z]{2,6}(\.[a-z]{2,4}){1,2}$"
     number_re=re.compile(pattern)
     #pdb.set_trace()
-    with open('data.txt', 'w') as outfile:
-         json.dump(newData, outfile)
     if number_re.findall(mail) and Question:
+        if os.stat('data.txt').st_size != 0:
+            with open('data.txt') as f:
+                Hystory = json.load(f) 
+        Hystory[mail] = Question
+        with open('data.txt', 'w') as outfile:
+             json.dump(Hystory, outfile)
         return "Thanks! The answer will be sent to the mail %s" % mail
     else:
         return "Error, make suer the mail is entered correctly and that all fields are filled"
