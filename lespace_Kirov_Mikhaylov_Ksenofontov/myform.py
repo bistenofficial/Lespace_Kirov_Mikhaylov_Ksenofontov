39e4056c86460ffd84465a0a4e4c5cf32017ab77
Ddkaba <Danimi2506@gmail.com>
2021-04-15 14:46:08 +0300

    JSON

--- lespace_Kirov_Mikhaylov_Ksenofontov/myform.py
+++ lespace_Kirov_Mikhaylov_Ksenofontov/myform.py
@@ -1,17 +1,29 @@
 import re
+import pdb
+import json
+import os
 from bottle import post, request 
 @post('/home', method='post')
 def my_form():
-    pattern = r'^\w+[@]\D{2,6}\.\D{2,4}$'
+    questions = {}
+    pattern = r'^\w+[@](\D{2,6})\.(\D{1-10}\.\D{2,4)|(\D{2,4})$'
     mail = request.forms.get('ADRESS')
-    question = request.forms.get('QUEST')
+    text = request.forms.get('QUEST')
     number_re=re.compile(pattern)
-    if(question == ""):
+    if(text == ""):
         if(mail == ""):
             return "Fill in both fields"
         else: return "Fill in the question field"
-    else: 
+    else:
         if(mail == ""):
             return "Fill in mail field"
-    if(number_re.findall(mail)): return "Thanks! The answer will be sent to the mail %s" %mail
+    if(number_re.findall(mail)):
+        if(os.stat('C:/Users/79522/Desktop/data.txt').st_size !=0):
+            with open ('C:/Users/79522/Desktop/data.txt') as fr:
+                questions = json.load(fr)
+                fr.close
+        with open ('C:/Users/79522/Desktop/data.txt','w') as outfile:
+            questions[mail] = text
+            json.dump(questions,outfile)
+        return "Thanks! The answer will be sent to the mail %s" %mail
     else: return "Mail does not match format"
