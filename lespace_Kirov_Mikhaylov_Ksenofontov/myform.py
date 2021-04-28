b15f9ffd457c55dcb81fa3de013a8bba077e68da
bistenofficial <maks.kirov.2002@mail.ru>
2021-04-28 22:55:49 +0300

    7-8 done

--- lespace_Kirov_Mikhaylov_Ksenofontov/myform.py
+++ lespace_Kirov_Mikhaylov_Ksenofontov/myform.py
@@ -1,12 +1,23 @@
 from bottle import post, request, re
+import pdb
+import json
+
+questions ={}
+questions['question'] =[]
 
 @post('/home', method='post')
 def my_form():
     reg = re.compile(r'\w+@\w+\.\w')
+
     mail = request.forms.get('ADRESS')
-    if(mail=="" or request.forms.get('QUEST'=="")):
+    data = request.forms.get('QUEST')
+    if(mail=="" or data==""):
         return "Some field is empty"
     elif reg.match(mail):
+        ###pdb.set_trace()
+        questions['question'].append({'email':mail,'ask':data})
+        with open('questions.txt','w') as outfile:
+            json.dump(questions, outfile)
         return "Thanks! The answer will be sent to the mail %s" % mail
     else:
         return "Mail input error"
