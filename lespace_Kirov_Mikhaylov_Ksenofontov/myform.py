4e951416f17b46ffc18886130de51c735842263c
Sergey <ksenof.serrr@mail.ru>
2021-04-28 19:25:46 +0300

    Reworked myform

--- lespace_Kirov_Mikhaylov_Ksenofontov/myform.py
+++ lespace_Kirov_Mikhaylov_Ksenofontov/myform.py
@@ -5,6 +5,11 @@ import re
 #import pdb
 @post('/home', method='post')
 
+def reg_check(str):
+    pattern=r"^\w+[@][a-z]{2,6}(\.[a-z]{2,4}){1,2}$"
+    number_re=re.compile(pattern)
+    return bool(number_re.findall(str))
+
 def my_form():
     mail = request.forms.get('ADRESS')
     Question = request.forms.get('QUEST')
@@ -14,12 +19,10 @@ def my_form():
     with open('data.txt') as f:
         data = json.load(f)
     newData = data.update(Hystory)
-    pattern=r"^\w+[@][a-z]{2,6}(\.[a-z]{2,4}){1,2}$"
-    number_re=re.compile(pattern)
     #pdb.set_trace()
     with open('data.txt', 'w') as outfile:
          json.dump(newData, outfile)
-    if number_re.findall(mail) and Question:
+    if reg_check(mail) and Question:
         return "Thanks! The answer will be sent to the mail %s" % mail
     else:
         return "Error, make suer the mail is entered correctly and that all fields are filled"
